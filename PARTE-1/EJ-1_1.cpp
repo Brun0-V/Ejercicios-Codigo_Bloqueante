a562b6d148203d49cbeb53ba68732722a7410ff8
Bruno <93298185+Brun0-V@users.noreply.github.com>
2023-06-27 18:14:05 -0300

    Add files via upload

--- PARTE-1/EJ-1_1.cpp
+++ PARTE-1/EJ-1_1.cpp
@@ -3,31 +3,28 @@
 #define LED2 5
 #define ON 1
 #define OFF 0
-//1 p0, a500
-//2 p0 a125, p250 a375, p500 a 625, p750 a875 
 void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
 }
 
 void loop() {
-  //0
   digitalWrite(LED1, ON);
   digitalWrite(LED2, ON);
-  delay(125);//125
+  delay(125);
   digitalWrite(LED2, OFF);
-  delay(125);//250
+  delay(125);
   digitalWrite(LED2, ON);
-  delay(125);//375
+  delay(125);
   digitalWrite(LED2, OFF);
-  delay(125);//500
+  delay(125);
   digitalWrite(LED1, OFF);
   digitalWrite(LED2, ON);
-  delay(125);//625
+  delay(125);
   digitalWrite(LED2, OFF);
-  delay(125);//750
+  delay(125);
   digitalWrite(LED2, ON);
-  delay(125);//875
+  delay(125);
   digitalWrite(LED2, OFF);
-  delay(125);//1000
+  delay(125);
 }
